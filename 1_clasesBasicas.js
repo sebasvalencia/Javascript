f6ed4fe34237ef3eca1e91da477a8113f88e79c0
Sebastian Valencia <sebastian.valencian@gmail.com>
2017-09-21 18:03:21 -0500

    Se crean archivos y 1_clasesBasicas

---
+++ 1_clasesBasicas.js
@@ -0,0 +1,17 @@
+
+class Rectangulo{
+    constructor(alto, largo){
+        this.alto = alto;
+        this.largo = largo;
+    }
+
+    getArea(){
+        return this.alto * this.largo;
+    }
+}
+
+let rectangulo = new Rectangulo(2,3);
+console.log(`El area del rectangulo es = ${rectangulo.getArea()}`);
+
+
+
